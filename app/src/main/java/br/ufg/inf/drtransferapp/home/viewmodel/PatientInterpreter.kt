cbffc63e1d4510844256e391e1549e87ca0ea721
Sergio Cabral <scastrocabral@gmail.com>
2025-03-03 00:39:49 -0300

    add: novas classes filhas para a classe PatientInterpreter para cada chamada de api específica

--- app/src/main/java/br/ufg/inf/drtransferapp/home/viewmodel/PatientInterpreter.kt
+++ app/src/main/java/br/ufg/inf/drtransferapp/home/viewmodel/PatientInterpreter.kt
@@ -1,7 +1,10 @@
 package br.ufg.inf.drtransferapp.home.viewmodel
 
-import br.ufg.inf.drtransferapp.home.model.PatientResponseModel
+import br.ufg.inf.drtransferapp.home.model.PatientRequestModel
 
 sealed class PatientInterpreter {
     object CallListPatientsApi : PatientInterpreter()
+    class CallCreatePatientApi(val patient: PatientRequestModel) : PatientInterpreter()
+    class CallUpdatePatientApi(val idPatient: String, val patient: PatientRequestModel) : PatientInterpreter()
+    class CallDeletePatientApi(val idPatient: String) : PatientInterpreter()
 }
