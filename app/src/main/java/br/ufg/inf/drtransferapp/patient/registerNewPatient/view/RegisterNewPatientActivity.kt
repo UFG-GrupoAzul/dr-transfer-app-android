bbfba8b8a44b166d15e599aea24f85a235bbfda5
Sergio Cabral <scastrocabral@gmail.com>
2025-03-03 15:12:30 -0300

    mod: ajuste layout do casdastrar novo paciente e adiciona lógica na activity para tratar opções de tipo sangue e genero

--- app/src/main/java/br/ufg/inf/drtransferapp/patient/registerNewPatient/view/RegisterNewPatientActivity.kt
+++ app/src/main/java/br/ufg/inf/drtransferapp/patient/registerNewPatient/view/RegisterNewPatientActivity.kt
@@ -1,6 +1,9 @@
 package br.ufg.inf.drtransferapp.patient.registerNewPatient.view
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.MultiAutoCompleteTextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,10 +13,13 @@ import androidx.lifecycle.ViewModelProvider
 import br.ufg.inf.drtransferapp.R
 import br.ufg.inf.drtransferapp.databinding.ActivityRegisterNewPatientBinding
 import br.ufg.inf.drtransferapp.patient.commons.model.PatientRequestModel
+import br.ufg.inf.drtransferapp.patient.commons.utils.enum.Genero
+import br.ufg.inf.drtransferapp.patient.commons.utils.enum.TipoSanguineo
 import br.ufg.inf.drtransferapp.patient.registerNewPatient.viewmodel.RegisterNewPatientFactory
 import br.ufg.inf.drtransferapp.patient.registerNewPatient.viewmodel.RegisterNewPatientInterpreter
 import br.ufg.inf.drtransferapp.patient.registerNewPatient.viewmodel.RegisterNewPatientStates
 import br.ufg.inf.drtransferapp.patient.registerNewPatient.viewmodel.RegisterNewPatientVM
+import com.google.android.material.textfield.TextInputLayout
 
 class RegisterNewPatientActivity : AppCompatActivity() {
     private val binding: ActivityRegisterNewPatientBinding by lazy {
@@ -21,6 +27,9 @@ class RegisterNewPatientActivity : AppCompatActivity() {
             layoutInflater
         )
     }
+
+    private lateinit var autoCompleteTipoSangue: AutoCompleteTextView
+    private lateinit var autoCompleteGenero: AutoCompleteTextView
 
     private val viewmodel: RegisterNewPatientVM by lazy {
         ViewModelProvider(
@@ -39,8 +48,30 @@ class RegisterNewPatientActivity : AppCompatActivity() {
             insets
         }
 
+        iniViews()
         setClickListener()
         initObserver()
+    }
+
+    private fun iniViews() {
+        autoCompleteTipoSangue = binding.autoCompleteTipoSangue
+        val options= listOf(
+            "A-Positivo",
+            "A-Negativo",
+            "B-Positivo",
+            "B-Negativo",
+            "AB-Positivo",
+            "AB-Negativo",
+            "O-Positivo",
+            "O-Negativo"
+        )
+        val adpater = ArrayAdapter(this, android.R.layout.simple_list_item_1, options)
+        autoCompleteTipoSangue.setAdapter(adpater)
+
+        autoCompleteGenero = binding.autoCompleteGenero
+        val optionsGenero = listOf("Masculino", "Feminino")
+        val adpaterGenero = ArrayAdapter(this, android.R.layout.simple_list_item_1, optionsGenero)
+        autoCompleteGenero.setAdapter(adpaterGenero)
     }
 
     private fun initObserver() {
@@ -66,7 +97,8 @@ class RegisterNewPatientActivity : AppCompatActivity() {
         binding.btnConfirmar.setOnClickListener {
             if (binding.etNome.text.isNullOrBlank() || binding.etDataAniversario.text.isNullOrBlank()
                 || binding.etTelefone.text.isNullOrBlank() || binding.etCpf.text.isNullOrBlank()
-                || binding.etGenero.text.isNullOrBlank() || binding.etTipoSangue.text.isNullOrBlank()
+                || isItemDropdownSelected(autoCompleteGenero,binding.tfGenero) && autoCompleteGenero.text.toString().isBlank()
+                || isItemDropdownSelected(autoCompleteTipoSangue,binding.tfTipoSangue) && autoCompleteTipoSangue.text.toString().isBlank()
             ) {
                 Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
             } else {
@@ -74,8 +106,8 @@ class RegisterNewPatientActivity : AppCompatActivity() {
                 val dataAniversario = binding.etDataAniversario.text.toString()
                 val telefone = binding.etTelefone.text.toString()
                 val cpf = binding.etCpf.text.toString()
-                val genero = binding.etGenero.text.toString().uppercase()
-                val tipoSangue = binding.etTipoSangue.text.toString()
+                val genero = fixGender(binding.autoCompleteGenero.text.toString())
+                val tipoSangue = fixBloodType(binding.autoCompleteTipoSangue.text.toString())
 
                 val patient =
                     PatientRequestModel(
@@ -89,6 +121,46 @@ class RegisterNewPatientActivity : AppCompatActivity() {
 
                 viewmodel.interpret(RegisterNewPatientInterpreter.CallCreatePatientApi(patient))
             }
+        }
+
+        autoCompleteTipoSangue.setOnClickListener {
+            autoCompleteTipoSangue.showDropDown()
+        }
+
+        autoCompleteGenero.setOnClickListener {
+            autoCompleteGenero.showDropDown()
+        }
+    }
+
+    private fun isItemDropdownSelected(autoCompleteTextView: AutoCompleteTextView, textInputLayout: TextInputLayout): Boolean {
+        return if (autoCompleteTextView.text.toString().isNullOrBlank()) {
+            textInputLayout.error  = "Selecione uma opção"
+            true
+        } else {
+            textInputLayout.error = null
+            false
+        }
+    }
+
+    private fun fixBloodType(autoCompleteTipoSangue: String) : String {
+        return when (autoCompleteTipoSangue) {
+            "A-Positivo" -> TipoSanguineo.A_POSITIVE.toString()
+            "A-Negativo" -> TipoSanguineo.A_NEGATIVE.toString()
+            "B-Positivo" -> TipoSanguineo.B_POSITIVE.toString()
+            "B-Negativo" -> TipoSanguineo.B_NEGATIVE.toString()
+            "AB-Positivo" -> TipoSanguineo.AB_POSITIVE.toString()
+            "AB-Negativo" -> TipoSanguineo.AB_NEGATIVE.toString()
+            "O-Positivo" -> TipoSanguineo.O_POSITIVE.toString()
+            "O-Negativo" -> TipoSanguineo.O_NEGATIVE.toString()
+            else -> ""
+        }
+    }
+
+    private fun fixGender(autoCompleteGenero: String): String {
+        return when (autoCompleteGenero) {
+            "Masculino" -> Genero.MALE.toString()
+            "Feminino" -> Genero.FAMALE.toString()
+            else -> ""
         }
     }
 }
